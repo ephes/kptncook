545f3ec866390f8dacd92b6be3212e029afa0a11
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-09-13 16:48:26 +0200

    #23 don't break the complete export if one recipe is not valid -> just skip

--- kptncook/__init__.py
+++ kptncook/__init__.py
@@ -79,16 +79,16 @@ def get_kptncook_recipes_from_repository() -> list[Recipe]:
     fs_repo = RecipeRepository(settings.root)
     recipes = []
     for repo_recipe in fs_repo.list():
-        recipes.append(Recipe.model_validate(repo_recipe.data))
-        # try:
-        #     recipes.append(Recipe.model_validate(repo_recipe.data))
-        # except Exception as e:
-        #     print(f"Could not parse recipe {repo_recipe.id}: {e}")
-        #     for ingredient in repo_recipe.data.get("ingredients"):
-        #         uncountable_title = ingredient["ingredient"].get("uncountableTitle")
-        #         if uncountable_title is None:
-        #             print("ingredient: ", ingredient["ingredient"])
-        #     return []
+        # recipes.append(Recipe.model_validate(repo_recipe.data))
+        try:
+            recipes.append(Recipe.model_validate(repo_recipe.data))
+        except Exception as e:
+            print(f"Could not parse recipe {repo_recipe.id}: {e}")
+            for ingredient in repo_recipe.data.get("ingredients"):
+                uncountable_title = ingredient["ingredient"].get("uncountableTitle")
+                if uncountable_title is None:
+                    print("ingredient: ", ingredient["ingredient"])
+            # return []
     return recipes
 
 
