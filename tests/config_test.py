7ac36aca33a6bcc9d62f6423c19c188326609192
Felix Berlakovich <felix@berlakovich.at>
2025-01-02 10:28:39 +0100

    Simplify tests

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,22 +1,21 @@
 import pydantic
 import pytest
 
-from kptncook.config import Settings
-from tests.conftest import temp_env
+from tests.conftest import MockSettings
 
 
 def test_config_accepts_mealie_username_password():
-    with temp_env(MEALIE_USERNAME="test", MEALIE_PASSWORD="test", KPTNCOOK_API_KEY="test"):
-        settings = Settings()
+    MockSettings(
+        mealie_username="test", mealie_password="password", kptncook_api_key="test"
+    )
 
 
 def test_config_accepts_mealie_token():
-    with temp_env(MEALIE_API_TOKEN="test", KPTNCOOK_API_KEY="test"):
-        settings = Settings()
+    MockSettings(mealie_api_token="test", kptncook_api_key="test")
+
 
 def test_config_rejects_empty_mealie_auth():
     with pytest.raises(pydantic.ValidationError) as exception_info:
-        with temp_env(KPTNCOOK_API_KEY="test"):
-            settings = Settings()
+        MockSettings(kptncook_api_key="test")
 
-    assert "mealie" in str(exception_info.value)
+    assert "must specify either" in str(exception_info.value)
